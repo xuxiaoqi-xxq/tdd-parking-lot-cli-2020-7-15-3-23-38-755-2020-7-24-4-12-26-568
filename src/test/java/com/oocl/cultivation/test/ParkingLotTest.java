b71486825712a9fceebaee308e57f251984cd424
xuxiaoqi-xxq <xxq_work@163.com>
2020-07-24 15:35:01 +0800

    [1][eva] feature: add test should_return_car_ticket_when_park_given_car

---
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
@@ -0,0 +1,24 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.CarTicket;
+import com.oocl.cultivation.ParkingLot;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ParkingLotTest {
+
+    @Test
+    void should_return_car_ticket_when_park_given_car() {
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+
+        //when
+        CarTicket carTicket = parkingLot.park(car);
+
+        //then
+        assertNotNull(carTicket);
+    }
+}
