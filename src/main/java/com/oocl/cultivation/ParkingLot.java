ef4a75e7eaf239441b400f007dc882a720718a20
xuxiaoqi-xxq <xxq_work@163.com>
2020-07-24 15:39:52 +0800

    [1][eva] feature: add test should_fetch_car_when_fetch_car_given_car_ticket

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -4,4 +4,8 @@ public class ParkingLot {
     public CarTicket park(Car car) {
         return new CarTicket();
     }
+
+    public Car fetch(CarTicket carTicket) {
+        return null;
+    }
 }
--- src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotTest.java
@@ -5,6 +5,7 @@ import com.oocl.cultivation.CarTicket;
 import com.oocl.cultivation.ParkingLot;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotTest {
@@ -20,5 +21,20 @@ public class ParkingLotTest {
 
         //then
         assertNotNull(carTicket);
+    }
+
+    @Test
+    void should_fetch_car_when_fetch_car_given_car_ticket() {
+        //given
+        CarTicket carTicket = new CarTicket();
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+
+        //when
+        Car fetchedCar = parkingLot.fetch(carTicket);
+
+        //then
+        assertNotNull(fetchedCar);
+        assertEquals(car, fetchedCar);
     }
 }
