411c2553c16a395ea890d39dc42ccd0158b5a9a4
xxq <xxq_work@163.com>
2020-07-26 12:35:15 +0800

    [2][eva] feature: implement when fetch car given wrong car ticket will return error msg

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,4 +1,19 @@
 package com.oocl.cultivation;
 
 public class ParkingBoy {
+
+
+    private ParkingLot parkingLot;
+
+    public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    public CarTicket park(Car car) {
+        return null;
+    }
+
+    public Object fetch(CarTicket carTicket) {
+        return "Unrecognized parking ticket.";
+    }
 }
--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -1,4 +1,26 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.CarTicket;
+import com.oocl.cultivation.ParkingBoy;
+import com.oocl.cultivation.ParkingLot;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ParkingBoyTest {
+
+    @Test
+    void should_return_error_msg_when_fetch_car_given_wrong_car_ticket() {
+        //given
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
+        parkingBoy.park(new Car());
+        CarTicket wrongCarTicket = new CarTicket();
+
+        //when
+        Object fetched = parkingBoy.fetch(wrongCarTicket);
+
+        //then
+        assertEquals("Unrecognized parking ticket.", fetched);
+    }
 }
