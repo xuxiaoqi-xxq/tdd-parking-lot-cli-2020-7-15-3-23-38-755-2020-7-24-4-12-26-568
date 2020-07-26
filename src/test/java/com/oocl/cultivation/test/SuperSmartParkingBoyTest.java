74de705db41f035881c421ecb33cf3d58c6ba1dc
xxq <xxq_work@163.com>
2020-07-26 14:49:38 +0800

    [5][eva] feature: implement when park will return error msg when parking lots full

--- src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SuperSmartParkingBoyTest {
@@ -28,5 +29,28 @@ public class SuperSmartParkingBoyTest {
 
         // then
         assertTrue(carTicket instanceof CarTicket);
+    }
+
+    @Test
+    void should_return_error_msg_when_all_parking_lots_full_given_car() throws NoSuchFieldException {
+        // given
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLotOne = new ParkingLot();
+        for (int i = 0; i < 10; i++) {
+            parkingLotOne.park(new Car());
+        }
+        ParkingLot parkingLotTwo = new ParkingLot();
+        for (int i = 0; i < 10; i++) {
+            parkingLotTwo.park(new Car());
+        }
+        parkingLots.add(parkingLotOne);
+        parkingLots.add(parkingLotTwo);
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+
+        // when
+        Object parkResult = superSmartParkingBoy.park(new Car());
+
+        // then
+        assertEquals("Not enough position.", parkResult);
     }
 }
