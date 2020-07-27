80fe81b4344ba6d5e5891ae52d9b1a49f4f85c31
xuxiaoqi-xxq <xxq_work@163.com>
2020-07-27 16:58:04 +0800

    [4][eva] feature: modify assertTrue to assertEquals in should_park_into_more_empty_parking_lot_when_park_given_car

--- src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class SmartParkingBoyTest {
 
     @Test
-    void should_park_into_more_empty_parking_lot_when_park_given_car() throws NoSuchFieldException {
+    void should_park_into_more_empty_parking_lot_when_park_given_car() {
         // given
         List<ParkingLot> parkingLots = new ArrayList<>();
         ParkingLot parkingLotOne = new ParkingLot();
@@ -29,14 +29,14 @@ public class SmartParkingBoyTest {
         SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
 
         // when
-        CarTicket carTicket = (CarTicket)smartParkingBoy.park(new Car());
+        smartParkingBoy.park(new Car());
 
         // then
-        assertTrue(carTicket instanceof CarTicket);
+        assertEquals(8, parkingLotOne.getEmptyPosition());
     }
 
     @Test
-    void should_return_error_msg_when_all_parking_lots_full_given_car() throws NoSuchFieldException {
+    void should_return_error_msg_when_all_parking_lots_full_given_car() {
         // given
         List<ParkingLot> parkingLots = new ArrayList<>();
         ParkingLot parkingLotOne = new ParkingLot();
