21a1246710ea3d20388897859c3d10ef40c9d03d
xuxiaoqi-xxq <xxq_work@163.com>
2020-07-27 17:58:34 +0800

    [4][eva] feature: modify SmartParkingBoyTest to adapt park result as ParkResult

--- src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,39 +16,28 @@ public class SmartParkingBoyTest {
     @Test
     void should_park_into_more_empty_parking_lot_when_park_given_car() {
         // given
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        ParkingLot parkingLotOne = new ParkingLot(10);
-        parkingLotOne.park(new Car());
-        ParkingLot parkingLotTwo = new ParkingLot(10);
-        parkingLotTwo.park(new Car());
-        parkingLotTwo.park(new Car());
-        parkingLots.add(parkingLotOne);
-        parkingLots.add(parkingLotTwo);
-        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        ParkingLot parkingLotOne = new ParkingLot(1);
+        ParkingLot parkingLotTwo = new ParkingLot(2);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(Arrays.asList(parkingLotOne, parkingLotTwo));
 
         // when
         smartParkingBoy.park(new Car());
 
         // then
-        assertEquals(8, parkingLotOne.getEmptyPosition());
+        assertEquals(1, parkingLotOne.getEmptyPosition());
+        assertEquals(1, parkingLotTwo.getEmptyPosition());
     }
 
     @Test
     void should_return_error_msg_when_all_parking_lots_full_given_car() {
         // given
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        ParkingLot parkingLotOne = new ParkingLot(1);
-        parkingLotOne.park(new Car());
-        parkingLots.add(parkingLotOne);
-        ParkingLot parkingLotTwo = new ParkingLot(1);
-        parkingLotTwo.park(new Car());
-        parkingLots.add(parkingLotTwo);
-        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(Arrays.asList(new ParkingLot(1)));
+        smartParkingBoy.park(new Car());
 
         // when
-        Object parkResult = smartParkingBoy.park(new Car());
+        ParkResult parkResult = smartParkingBoy.park(new Car());
 
         // then
-        assertEquals("Not enough position.", parkResult);
+        assertEquals("Not enough position.", parkResult.getMessage());
     }
 }
